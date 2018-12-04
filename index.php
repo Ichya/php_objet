26d20ab57b6a8a663da1a724e71b6dcbfd23e1ba
Ichya <emerick.giraudineau@imie.fr>
2018-12-04 11:45:58 +0100

    Mise a jour index

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-var_dump("coucou")
+class Person {
+    private $firstname;
+    private $lastname;
+
+    public function setID($pFirstname, $pLastname) {
+        $this->firstname = $pFirstname;
+        $this->lastname = $pLastname;
+    }
+
+}
+
+
+$person1 = new Person();
+
+$person1->setID("John", "Poupe");
+
+$person2 = new Person();
+
+$person2->setID("poupe", "oupapipe");
+
+
+
+
+var_dump($person1);
+
+var_dump($person2);
